0ee65807af5805fb341409eaa0a1c6c5bfbd7b28
warham123 <warhamaliansa23@gmail.com>
2021-12-07 18:51:57 +0800

    Membikin program untuk garis Horizontal

--- projeck.py
+++ projeck.py
@@ -11,34 +11,48 @@ N = x2-x1+1
 x = x1
 
 if x1 == x2:
-     print("Garis vertical")
-#ini di kerja
+    print("Garis vertical")
+# ini di kerja
 elif y1 == y2:
-    print("garis horizontal")
-#ini juga
+    y = y1 = y2
+    titikx = []
+    titikya = []
+    print("Garis vertical")
+    print("-----------------------------")
+
+    while x <= x2:
+        print("Garis Yang dilewati adalah (", x, ",", y, ")")
+        titikx.append(x)
+        titikya.append(y)
+        x = x+1
+
+    plt.plot(titikx, titikya)
+    plt.show()
+
+
 else:
-    titikx=[]
-    titikya=[]
-    print("Nilai kemiringannya adalah m = ",m)
-    print("Nilai N adalah N = ",N)
-    print("Nilai X adalah x = ",x)
+    titikx = []
+    titikya = []
+    print("Nilai kemiringannya adalah m = ", m)
+    print("Nilai N adalah N = ", N)
+    print("Nilai X adalah x = ", x)
     print("____________________________")
-    
-    i=0
+
+    i = 0
     while i < N:
         x = x1 + i
-        j=0
+        j = 0
         while j <= i:
-             y = m*(x-x1)+y1
-             ya = round(y)
-             j=j+1
-        print ("Nilai x nya adalah = ",x)
-        print ("nilai Y adalah = ", y)
-        print ("nilai Ya adalah = ",ya)
-        print ("Garis yang dilewati adalah (",x,",",ya,")")
-        print ("-----------------------------")
+            y = m*(x-x1)+y1
+            ya = round(y)
+            j = j+1
+        print("Nilai x nya adalah = ", x)
+        print("nilai Y adalah = ", y)
+        print("nilai Ya adalah = ", ya)
+        print("Garis yang dilewati adalah (", x, ",", ya, ")")
+        print("-----------------------------")
         titikx.append(x)
         titikya.append(ya)
-        i = i+1 
-    plt.plot(titikx,titikya)
+        i = i+1
+    plt.plot(titikx, titikya)
     plt.show()
